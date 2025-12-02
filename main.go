e02848bb6e1ba31f5cc2a58f88042d4912ceaa73
Sean Neale <sean_neale@hotmail.co.uk>
2025-12-02 14:58:37 +0800

    enhance/transitland_test: set up env vars; test transitland api

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	// handle routes
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 
 	// Start the server
-	err := http.ListenAndServe(":4001", mux)
+	err = http.ListenAndServe(":4001", mux)
 	if err != nil {
 		log.Fatal("Error occurred while starting the server:", err)
 	}
