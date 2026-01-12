3c24a110f09b2c1f93cbf3614978e0bbb73e175d
Sean Neale <sean_neale@hotmail.co.uk>
2026-01-12 16:21:09 +0800

    refactor/fixed_template: refactor handler methods and page templates

--- handlers.go
+++ handlers.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+type Table struct {
+	Name    string
+	BusTime []busTime
+}
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/home.html")
 	if err != nil {
@@ -15,51 +20,39 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleTsoWoHang(w http.ResponseWriter, r *http.Request) {
-	type response struct {
-		TsoWoHang, MakPin []busTime
+	tables := []Table{
+		{Name: "Tso Wo Hang", BusTime: kmb_bus_times([]string{"B959226950B0DEA7"})},
+		{Name: "Mak Pin (Towards Ma On Shan)", BusTime: kmb_bus_times([]string{"B2F4485FA517FEED"})},
+		{Name: "Mak Pin (Towards Sai Kung)", BusTime: kmb_bus_times([]string{"C7548A3C37ADC1AA"})},
 	}
 
-	tmpl, err := template.ParseFiles("templates/tsowohang.html")
-	if err != nil {
-		http.Error(w, "Error loading home page", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, response{TsoWoHang: kmb_bus_times([]string{"B959226950B0DEA7"}), MakPin: kmb_bus_times([]string{"B2F4485FA517FEED", "C7548A3C37ADC1AA"})})
-	if err != nil {
-		return
-	}
+	renderFixedTable(tables, w, r)
 }
 
 func handleLearningCenter(w http.ResponseWriter, r *http.Request) {
-	type response struct {
-		ShaKokMei, HongKongAcademy []busTime
+	tables := []Table{
+		{Name: "Sha Kok Mei", BusTime: kmb_bus_times([]string{"F85F7F6FEB0812B5"})},
+		{Name: "Hong Kong Academy (8 Min Walk)", BusTime: kmb_bus_times([]string{"1AE7D87716BC52B1"})},
 	}
 
-	tmpl, err := template.ParseFiles("templates/learningcenter.html")
-	if err != nil {
-		http.Error(w, "Error loading home page", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, response{ShaKokMei: kmb_bus_times([]string{"F85F7F6FEB0812B5"}), HongKongAcademy: kmb_bus_times([]string{"1AE7D87716BC52B1"})})
-	if err != nil {
-		return
-	}
+	renderFixedTable(tables, w, r)
 }
 
 func handleAKungKok(w http.ResponseWriter, r *http.Request) {
-	type response struct {
-		AKungKok []busTime
+	tables := []Table{
+		{Name: "A Kung Kok", BusTime: kmb_bus_times([]string{"4B0E21EDF07F9C83", "B8BBCCA288E1F862", "6F03E19C5E800893"})},
 	}
+	renderFixedTable(tables, w, r)
+}
 
-	tmpl, err := template.ParseFiles("templates/akungkok.html")
+func renderFixedTable(tables []Table, w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/fixed_table.html")
 	if err != nil {
 		http.Error(w, "Error loading home page", http.StatusInternalServerError)
 		return
 	}
 
-	err = tmpl.Execute(w, response{AKungKok: kmb_bus_times([]string{"4B0E21EDF07F9C83", "B8BBCCA288E1F862", "6F03E19C5E800893"})})
+	err = tmpl.Execute(w, tables)
 	if err != nil {
 		return
 	}
